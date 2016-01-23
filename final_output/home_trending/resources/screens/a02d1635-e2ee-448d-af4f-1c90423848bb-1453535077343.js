79923a57e67ffa9ac9c83fb7d8f6f722cbc9720d
ramwert <ramranji@ramanarayanans-MacBook-Pro.local>
2016-01-23 02:46:04 -0500

    trending_tab

---
+++ final_output/home_trending/resources/screens/a02d1635-e2ee-448d-af4f-1c90423848bb-1453535077343.js
@@ -0,0 +1,123 @@
+jQuery("#simulation")
+  .on("click", ".s-a02d1635-e2ee-448d-af4f-1c90423848bb .click", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#s-Label-blue")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/d12245cc-1680-458d-89dd-4f0d7fb22724",
+                    "transition": "fade"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Label-blue_1")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/d7b5174c-f6d9-4f1c-9e6c-7b1480edb037",
+                    "transition": "fade"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Label-blue_2")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimShow",
+                  "parameter": {
+                    "target": "#s-nav_bar",
+                    "effect": {
+                      "type": "blind",
+                      "easing": "easeInCubic",
+                      "duration": 500
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    }
+  })
+  .on("pageload", ".s-a02d1635-e2ee-448d-af4f-1c90423848bb .pageload", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#s-Label_99")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimSetValue",
+                  "parameter": {
+                    "target": "#s-Label_99",
+                    "value": {
+                      "action": "jimConcat",
+                      "parameter": [ {
+                        "action": "jimSubstring",
+                        "parameter": [ {
+                          "action": "jimSystemTime"
+                        },"0","5" ]
+                      }," PM" ]
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    }
+  });
